eb88082ad697adc8bef98b124df4e83d373128e3
DLRibeiro <dlr4@cin.ufpe.br>
2017-04-29 23:12:41 -0300

    Trocando a logica do simulador e estimador

--- src/Estimador/Estimador.java
+++ src/Estimador/Estimador.java
@@ -8,17 +8,15 @@ import java.util.Random;
  */
 public class Estimador implements Runnable {
     public int[] frame;
-    public Random rng;
-    public int qtd_etiquetas;
-    public int qtd_etiquetas_i;
     public String tipo;
 
-    public Estimador(int frameSize,int qtd_etiquetas,String tipo){
-        this.frame = new int[frameSize];
-        this.qtd_etiquetas = qtd_etiquetas;
-        this.qtd_etiquetas_i = 0;
-        this.rng = new Random();
+    public Estimador(int[] frame,String tipo){
+        this.frame = frame;
         this.tipo = tipo;
+    }
+
+    public void LowerBound(){
+
     }
 
     @Override
--- src/Simulador/DFSASimulator.java
+++ src/Simulador/DFSASimulator.java
@@ -1,16 +1,36 @@
 package Simulador;
 import Estimador.*;
+
+import java.util.Random;
+
 /**
  * Created by danil on 24/04/2017.
  */
-public class DFSASimulator {
+public class DFSASimulator implements Runnable{
     private int QTD_INICIAL_SLOTS;//Qtd inicial de slots
     private int QTD_INICIAL_TAGS;//Qtd inicial das tags
     private int INC_PASSOS;//Quantidade à ser incrementada por passo
     private int QTD_PASSOS;
     private int QTD_SIM_PASSO;//Qtd de simulações por passo
+    private Random rng;
+    private int counterFrames;
+    private int[] frame;
     private Estimador estimador;
-    public DFSASimulator(){
+    public DFSASimulator(int qtd_i_s, int qtd_i_t, int inc_passos, int passos, int qtd_sim_passo,int tamanho_inicial_frame,String tipo){
+        this.QTD_INICIAL_SLOTS = qtd_i_s;
+        this.QTD_INICIAL_TAGS = qtd_i_t;
+        this.INC_PASSOS = inc_passos;
+        this.QTD_PASSOS = passos;
+        this.QTD_SIM_PASSO = qtd_sim_passo;
+        this.frame = new int[tamanho_inicial_frame];
+        this.counterFrames = 1;
+        this.rng = new Random();
+        this.estimador = new Estimador(frame, tipo);
+    }
+
+
+    @Override
+    public void run() {
 
     }
 }
